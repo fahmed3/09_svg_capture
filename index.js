f588e81e9e8e5dde397beff217a0ed25e590a4dc
fahmed3 <fahmed3@stuy.edu>
2018-03-06 22:45:45 -0500

    finished

---
+++ index.js
@@ -0,0 +1,36 @@
+var s = document.getElementById("svg_id");
+var clr = document.getElementById("clr");
+
+var circle = function(e){
+    var radius = 25;
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", x);
+    circle.setAttribute("cy", y);
+    circle.setAttribute("r", radius);
+    circle.setAttribute("stroke", "black");
+    circle.addEventListener("click", colorChange);
+    s.appendChild(circle);    
+}
+
+var colorChange = function(e) {
+    //random color code from Gordon Lei
+    var fillStyle = "#" + Math.floor((Math.random()*16777215)).toString(16);
+    this.setAttribute("fill", fillStyle);
+    this.addEventListener("click", removeCircle);
+    e.stopPropagation();
+}
+
+var removeCircle = function() {
+    s.removeChild(this);
+}
+
+var clear = function(){
+    while(s.hasChildNodes()){
+	s.removeChild(s.lastChild);
+    }
+}
+
+clr.addEventListener("click", clear);
+s.addEventListener("click", circle);
